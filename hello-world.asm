add4ad0193871d3a726be0a0e696d4738c4730d7
jim_64 <36135907+jim-64@users.noreply.github.com>
2018-02-04 13:52:15 -0600

    Add files via upload

---
+++ hello-world.asm
@@ -0,0 +1,18 @@
+; This is a project just to see how GIT HUB works
+;
+;
+; COMPILE: cl65 -C c64-asm.cfg -u __EXEHDR__ hello-world.asm
+;
+	.macpack	cbm
+	.macpack	longbranch
+	
+	sym_CHROUT = $ffd2
+
+start:
+
+; clear screen
+	lda #34
+	jsr $ffd2
+	
+exit:	
+	rts
